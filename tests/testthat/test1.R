54920ea655e75cc645d088fd956340584326d890
Eliana Christou <echris15@uncc.edu>
2020-08-27 21:20:01 -0500

    remove the examples that produce plots.  retain the example that tests whether the function gives the error message

--- tests/testthat/test1.R
+++ tests/testthat/test1.R
@@ -1,17 +1,5 @@
 library(quantdr)
 
-test_that("Local linear estimation", {
-  set.seed(1234)
-  n <- 100
-  x <- rnorm(100); error <- rnorm(100); y <- x^2 + error
-  tau <- c(0.25, 0.5, 0.75)
-  par(mfrow=c(2,2))
-  for (i in 1:length(tau)) {
-    plot(x, y, main=print(tau[i]))
-    points(x, llqr(x, y, tau=tau[i])$ll_est, pch=16, col='red')
-  }
-})
-
 #test_that("Local linear estimation", {
 #  set.seed(1234)
 #  n <- 100
@@ -23,9 +11,9 @@ test_that("Local linear estimation", {
 test_that("the function gives an error message when the dimension of
           x0 is less than that of x", {
   set.seed(1234)
-  n <- 100
-  x <- cbind(rnorm(n), rnorm(n)); error <- rnorm(100); y <- x[, 1]^2 + error
+  n <- 100; p <- 2
+  x <- matrix(rnorm(n * p), n, p); error <- rnorm(n); y <- x[, 1]^2 + error
   tau <- 0.5
   x0 <- 1
-  expect_error(llqr(x, y, tau=tau, x0=x0)$ll_est, )
+  expect_error(llqr(x, y, tau = tau, x0 = x0)$ll_est, )
 })
