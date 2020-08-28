096ddcc6994c9ac1f8bf41ec91d164ffdda58082
Eliana Christou <echris15@uncc.edu>
2020-08-27 21:37:22 -0500

    add description, details, arguments, values, and polish the example for the llqrcv function

--- R/llqrcv.R
+++ R/llqrcv.R
@@ -1,12 +1,22 @@
-#' Cross-Validation for bandwidth selection for local linear
-#' conditional quantile estimation
+#' Cross-Validation bandwidth selection for local linear quantile estimation
 #'
-#' @param x a design matrix.The rows represent observations and the columns
-#'   represent predictor variables
-#' @param y a vector of the respons variable
-#' @param tau the quantile level, a number strictly between 0 and 1.
+#' \code{llqrcv} estimates the bandwidth necessary for the local linear fit of
+#' the \eqn{\tau}-th conditional quantile of \code{y} given \code{x}.  The
+#' estimation is performed using the Cross-Validation criterion.
+#'
+#' A grid of bandwidth values is created and the local linear fit is estimated
+#' using all the data points except for one point, which is used to make the
+#' prediction.  This procedure is repeated \code{n} times, where \code{n} is the
+#' number of observations.  Then, the averge error is computed using the check
+#' function \eqn{\rho_\tau(u)=(\tau - I(u<0))u}, where \eqn{\tau} the quantile
+#' level.  The bandwidth is selected as the one with the smallest average error.
+#'
+#' @param x A design matrix.  The rows represent observations and the columns
+#'   represent predictor variables.
+#' @param y A vector of the response variable.
+#' @param tau A quantile level, a number strictly between 0 and 1.
 #' @return \code{llqrcv} returns the optimal bandwidth selected using
-#'   Cross-Validation criterion for the estimation of the \code{tau}-th
+#'   Cross-Validation criterion for the local linear fit of the \eqn{\tau}-th
 #'   conditional quantile of \code{y} given \code{x}.
 #' @include llqr.R
 #' @examples
@@ -17,6 +27,7 @@
 #' @export
 llqrcv <- function(x, y, tau=0.5) {
   n <- length(y)
+  # create grid values for bandwidth
   h_lower <- min(n^ (- 1 / 5), min(2, sd(y)) * n^ (- 1 / 5))
   h_upper <- max(n^ (- 1 / 5), min(2, sd(y)) * n^ (- 1 / 5))
   h <- seq(h_lower, h_upper, by = 0.1)
@@ -24,7 +35,7 @@ llqrcv <- function(x, y, tau=0.5) {
   qhat <- matrix(0, n, length(h))
   cv <- as.null(length(h))
 
-  for (i in 1:length(h)) {
+  for (i in seq_len(h)) {
     for (j in 1:n) {
       qhat[j, i] <- llqr(x = x[-j, ], y = y[-j], tau = tau, h = h[i],
                          x0 = x[j, ])$ll_est
@@ -34,4 +45,3 @@ llqrcv <- function(x, y, tau=0.5) {
   h_opt <- h[which(cv == min(cv))]
   return(h_opt)
 }
-
