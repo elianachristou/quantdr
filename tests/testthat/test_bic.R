bc439ac033d3951497b1fdccec8da45993561143
Eliana Christou <echris15@uncc.edu>
2020-09-02 16:26:11 -0500

    separate the test files so that each file includes tests for each function

---
+++ tests/testthat/test_bic.R
@@ -0,0 +1,10 @@
+library(quantdr)
+
+test_that("the BIC criterion correctly estimates the dimension", {
+  set.seed(1234)
+  n <- 100; p <- 10; tau <- 0.5
+  x <- matrix(rnorm(n * p), n, p); error <- rnorm(n)
+  y <- 3 * x[, 1] + x[, 2] + error
+  out <- cqs(x, y, tau)
+  expect_equal(bic_d(out$qvalues, n, p), 1)
+})
---
+++ tests/testthat/test_llqr.R
@@ -0,0 +1,38 @@
+library(quantdr)
+
+test_that("the function returns an error when y is multivariate response", {
+  set.seed(1234)
+  n <- 100; p <- 2
+  x <- matrix(rnorm(n * p), n, p); error <- rnorm(n); y <- x^2 + error
+  tau <- 0.5
+  expect_error(llqr(x, y, tau = tau), )
+})
+
+test_that("the function returns an error when the number of observations
+          for y and x differ", {
+  set.seed(1234)
+  n <- 100; p <- 2
+  x <- matrix(rnorm(n * p), n, p); error <- rnorm(n); y <- x[, 1]^2 + error
+  tau <- 0.5
+  expect_error(llqr(x, y[1:(n/2)], tau = tau), )
+})
+
+test_that("the quantile level needs to be strictly between 0 and 1", {
+  set.seed(1234)
+  n <- 100
+  x <- rnorm(n); error <- rnorm(n); y <- (x + 1)^3 + 0.1 * (x - 2)^3 + error
+  tau <- -0.1
+  expect_error(llqr(x, y, tau = tau), )
+})
+
+
+test_that("the function gives an error message when the dimension of
+          x0 is less than that of x", {
+  set.seed(1234)
+  n <- 100; p <- 2
+  x <- matrix(rnorm(n * p), n, p); error <- rnorm(n); y <- x[, 1]^2 + error
+  tau <- 0.5
+  x0 <- 1
+  expect_error(llqr(x, y, tau = tau, x0 = x0)$ll_est, )
+})
+
