b2a9c46a1d45a58c7c46853b475950231083900a
Eliana Christou <echris15@uncc.edu>
2020-09-02 17:10:28 -0500

    add explanations about 'curse of dimensionality' problem in the details section

--- R/llqrcv.R
+++ R/llqrcv.R
@@ -1,30 +1,36 @@
-#' Cross-Validation for bandwidth selection of local linear quantile regression
+#'Cross-Validation for bandwidth selection of local linear quantile regression
 #'
-#' \code{llqrcv} estimates the bandwidth necessary for the local linear fit of
-#' the \eqn{\tau}th conditional quantile of \code{y} given \code{x}.  The
-#' estimation is performed using the Cross-Validation criterion.
+#'\code{llqrcv} estimates the bandwidth necessary for the local linear fit of
+#'the \eqn{\tau}th conditional quantile of \code{y} given \code{x}.  The
+#'estimation is performed using the Cross-Validation criterion.
 #'
-#' A grid of bandwidth values is created and the local linear fit is estimated
-#' using all the data points except for one point, which is used to make the
-#' prediction.  This procedure is repeated \code{n} times, where \code{n} is the
-#' number of observations.  Then, the bandwidth is selected as the one with the
-#' smallest average error.
+#'A grid of bandwidth values is created and the local linear fit is estimated
+#'using all the data points except for one point, which is used to make the
+#'prediction.  This procedure is repeated \code{n} times, where \code{n} is the
+#'number of observations.  Then, the bandwidth is selected as the one with the
+#'smallest average error.
 #'
-#' @param x A design matrix.  The rows represent observations and the columns
-#'   represent predictor variables.
-#' @param y A vector of the response variable.
-#' @param tau A quantile level, a number strictly between 0 and 1.
-#' @return \code{llqrcv} returns the optimal bandwidth selected using
-#'   Cross-Validation criterion for the local linear fit of the \eqn{\tau}th
-#'   conditional quantile of \code{y} given \code{x}.
-#' @include llqr.R
+#'When the dimension of the predictor variable is large, local linear fitting
+#'meets the 'curse of dimensionality' problem. In situations like that, the grid
+#'bandwidth values might be too small and cause the function to fail. For these
+#'cases, we advice the user to directly use the \code{llqr} function of the
+#'package and specify a bandwidth in the function.
+#'
+#'@param x A design matrix.  The rows represent observations and the columns
+#'  represent predictor variables.
+#'@param y A vector of the response variable.
+#'@param tau A quantile level, a number strictly between 0 and 1.
+#'@return \code{llqrcv} returns the optimal bandwidth selected using
+#'  Cross-Validation criterion for the local linear fit of the \eqn{\tau}th
+#'  conditional quantile of \code{y} given \code{x}.
+#'@include llqr.R
 #' @examples
 #' set.seed(1234)
 #' n <- 100
 #' x <- rnorm(100); error <- rnorm(100); y <- x^2 + error
 #' tau <- 0.5
 #' llqrcv(x, y, tau = tau)
-#' @export
+#'@export
 llqrcv <- function(x, y, tau=0.5) {
   x <- as.matrix(x)
 
